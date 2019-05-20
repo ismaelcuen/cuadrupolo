498cf21074219596251332e2ea650670305b120c
ismaelcuen <50840492+ismaelcuen@users.noreply.github.com>
2019-05-20 12:52:35 -0700

    Update campoelectrico.f90

--- campoelectrico.f90
+++ campoelectrico.f90
@@ -3,8 +3,6 @@ USE charged_particles
 USE vector_functions
   IMPLICIT NONE
   REAL,DIMENSION(3)::a,b
-  REAL,DIMENSION(3)::r
-  r=ABS(a%pos-b%pos)
-  a%Felec_d=(K*a%q_d*b%q_d)/r**2
-  a%Eelec_d=a%Felec_d/a%q_d
+  antiproton%Eelec=( k*proton%q / mag(sim%pos2)**2)*runit
+  antiproton%Felec = antiproton%q*antiproton%Eelec
 END SUBROUTINE campoelectrico
