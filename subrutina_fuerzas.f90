9fbb39799aefb89d3dc1fd817bb218ca9e8fb2d0
fjuzaino <41808092+fjuzaino@users.noreply.github.com>
2019-05-20 12:29:29 -0700

    Update and rename subrutina fuerzas to subrutina_fuerzas.f90

--- subrutina_fuerzas.f90
+++ subrutina_fuerzas.f90
@@ -1,5 +1,8 @@
 SUBROUTINE campoelectrico(a,b)
+USE charged_particles
+USE vector_functions
   IMPLICIT NONE
+  REAL,DIMENSION(3)::r
   r=ABS(a%pos-b%pos)
   a%Felec_d=(K*a%q*b%q)/r**2
 END SUBROUTINE campoelectrico
