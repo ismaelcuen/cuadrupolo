4ca6fbaa16e077eef92c47153eb95afb3b251e4b
fjuzaino <41808092+fjuzaino@users.noreply.github.com>
2019-05-20 12:30:13 -0700

    Update subrutina_fuerzas.f90

--- subrutina_fuerzas.f90
+++ subrutina_fuerzas.f90
@@ -2,6 +2,7 @@ SUBROUTINE campoelectrico(a,b)
 USE charged_particles
 USE vector_functions
   IMPLICIT NONE
+  REAL,DIMENSION(3)::a,b
   REAL,DIMENSION(3)::r
   r=ABS(a%pos-b%pos)
   a%Felec_d=(K*a%q*b%q)/r**2
