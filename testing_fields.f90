db5afc8652c45331b4305cc587a76232bd035387
fjuzaino <41808092+fjuzaino@users.noreply.github.com>
2019-05-20 13:14:30 -0700

    Update testing_fields.f90

--- testing_fields.f90
+++ testing_fields.f90
@@ -14,6 +14,7 @@ REAL(d) :: t !!Instante de tiempo en cada aproximaci´on
 REAL(d) :: scale=1E9 !!Escalar longitudes en los plots
 REAL ( d ), DIMENSION ( 3 ) :: runit !!Vector Unitario
 INTEGER :: unit0 !!Unidad para archivo de salida
+
 !!Asignando valores físicos a la particula en movimiento (MKS)
 antiproton%mass = 1.7E-27_d !!Masa del antiproton
 antiproton%q = -1.6E-19_d !!carga del antiproton
@@ -31,6 +32,7 @@ proton%pos = vector(-0.5E-8_d,0.0_d,0.0_d)
 !!Construyendo el vector de Momento lineal inicial del antiproton.
 antiproton%mom = antiproton%mass*antiproton%vel
 antiproton%Eelec = 0.0_d
+
 !!Ajustando el tama~no de paso (Cantidad de veces en las que se
 !!actualizar´a la informaci´on del sistema).
 sim%N_step = 5000 !!Usar´e s´olo 5000 pasos.
@@ -38,8 +40,10 @@ sim%ttot = 2E-11_d !!En este ejemplo se realizar´a la
 !!evoluci´on durante unos segundos
 !sim%ttot = sim%ttot/t0 !!Traduciendo a unidades naturales.
 sim%dt = sim%ttot/sim%N_step !!Calculando el ancho de paso
+
 !!Abriendo el archivo de texto
 OPEN(NEWUNIT=unit0,FILE="Sim_ejemplo",STATUS="UNKNOWN",ACCESS="APPEND")
+
 t=0 !!La simulaci´on inicia al tiempo t=0
 !Repetir el proceso hasta que se cumpla la condici´on
 DO
@@ -64,4 +68,5 @@ WRITE ( unit0 , * ) proton%pos*scale
 CLOSE ( unit0 )
 
 CALL SYSTEM("gnuplot -p data_plot.plt") !!Graficar los resultados
+
 END PROGRAM testing_fields
